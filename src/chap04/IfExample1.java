7cc27a0567de1e09e67af8cb4010c0aabd9af3b7
82103 <82103@DESKTOP-F2HLHM5>
2021-07-15 17:09:24 +0900

    hello

---
+++ src/chap04/IfExample1.java
@@ -0,0 +1,18 @@
+package chap04;
+
+public class IfExample1 {
+
+	public static void main(String[] args) {
+		int age = 8;
+		int price;
+		String str = "가나다";
+		switch(str) {
+		case "가나다" : System.out.println("8"); break;
+		case "나다다" : System.out.println("9"); break;
+		default : System.out.println("아무것두");break;
+			
+		}
+
+	}
+
+}
